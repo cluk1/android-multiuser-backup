d174785b5b68e9f1a5325e35d98504f52cd3e596
Christoph Lukas <lukas@pingworks.de>
2017-11-23 12:38:02 +0100

    added quotation for su calls

--- adb-restore.sh
+++ adb-restore.sh
@@ -4,12 +4,11 @@
 
 set -e
 
-adb shell su -c rm -rf $devdatadir
+adb shell su -c \'rm -rf $devdatadir\'
 adb push $localdatadir/ $devdatadir/
 adb push $(dirname $0)/restore.sh $devtmpdir/
 adb push $(dirname $0)/config $devtmpdir/
-adb shell su -c sh $devtmpdir/restore.sh $*
-adb shell su -c rm -rf $devdatadir
+adb shell su -c \'sh $devtmpdir/restore.sh $*\'
+adb shell su -c \'rm -rf $devdatadir\'
 adb shell rm -f $devtmpdir/restore.sh
 adb shell rm -f $devtmpdir/config
-
